3539e02f4b9004abf350b46fbb3dab5bee917f53
Belenko Ivan <belenko.ivan@inbox.ru>
2021-05-08 21:25:38 +0300

    start

---
+++ main.py
@@ -0,0 +1,10 @@
+from google_auth import *
+from manipulations import *
+
+account = "jterrell@auto-school.iam.gserviceaccount.com"
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
